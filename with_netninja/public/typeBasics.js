922c8101400ffda5a67c95cae2a0357a8d6b67a2
nathanbud <nthnlbdky916@gmail.com>
2020-06-09 00:00:22 +0800

    added basics

---
+++ with_netninja/public/typeBasics.js
@@ -0,0 +1,10 @@
+var character = 'mario';
+var age = 30;
+var isBlackBelt = false;
+character = 'dog';
+var circ = function (diameter) {
+    return diameter * Math.PI;
+};
+
+
+//You can change the value but not the type
---
+++ with_netninja/src/typeBasics.ts
@@ -0,0 +1,15 @@
+let character = 'mario';
+let age = 30;
+let isBlackBelt = false;
+
+ character = 'dog'
+
+const circ = (diameter) => {
+    return diameter * Math.PI
+}
+
+console.log(circ(12));
+
+
+
+//You can change the value but not the type
