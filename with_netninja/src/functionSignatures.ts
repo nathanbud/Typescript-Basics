b0ea0ed40bb3f4737ef9ba191a197a225dffcb2e
nathanbud <nthnlbdky916@gmail.com>
2020-06-11 23:31:44 +0800

    function signatures

---
+++ with_netninja/src/functionSignatures.ts
@@ -0,0 +1,13 @@
+let greetMe: Function;
+
+//Example 1
+let greetMe1: (a:string, b: string) => void;
+
+ greetMe1 = (name:"Nathan", greeting:"hahaha") => {
+console.log(`${name} says ${greeting}`);
+}
+
+//Example 2
+
+
+//Example 3
